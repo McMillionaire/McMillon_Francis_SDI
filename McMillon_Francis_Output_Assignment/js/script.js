4390367503f9de5b19f15e258e7ec1a8266dd188
McMillionaire <McMillionaire@fullsail.edu>
2015-03-05 22:43:38 -0500

    Output Assignment
    Introducing myself

---
+++ McMillon_Francis_Output_Assignment/js/script.js
@@ -0,0 +1,37 @@
+
+ /*
+  Francis McMillon
+  SDI
+  McMillon_Francis_Output_Assignment
+  3/5/15
+ */
+
+ var myIntro = "Hello, my name is Francis McMillon"; //My full name
+ var myIntro2 = "I served 20 years and 31 days in the US Army"; //How long I served in the military
+
+ alert(myIntro);//alert box showing my name
+ alert(myIntro2);//alert box showing how long I served in the military
+
+
+ var myIntro3 = "I moved to Norfolk, VA from Atlanta, GA in December 2011"; //text string
+ var myIntro4 = "Why?";//ask a question
+
+ alert(myIntro3);//alert box with text string
+ alert(myIntro4);//alert box with the question WHY?
+
+ myIntro5 = "To be with the LOVE of my Life!";//more text string
+ myIntro6 = "And the rest is HISTORY!";//more text string
+
+ alert(myIntro5);//alert box with text string
+ alert(myIntro6);//alert box with final text string
+
+ myIntro7 = "Let's test your memory";
+
+ alert(myIntro7);
+
+ var years = 20; // Number variable
+
+ var yearsServed = true; //Boolean Variable
+
+
+ console.log(myIntro);
