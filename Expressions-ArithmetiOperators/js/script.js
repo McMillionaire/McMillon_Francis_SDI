adfbadc58ceb6a32d318073a3a8f26fbbe59e166
McMillionaire <McMillionaire@fullsail.edu>
2015-03-11 19:47:10 -0400

    Arithmetic Operators
    Area of a Triangle

---
+++ Expressions-ArithmetiOperators/js/script.js
@@ -0,0 +1,14 @@
+
+ /*
+  Francis McMillon
+  SDI 1503
+ Arithmetic Operators (Adding it all up together
+  3/11/15
+ */
+
+ //area of the triangle is half of the width times the height
+
+ var width = 4;
+ var height = 5;
+ var area = width * height/2; //4 times 5 divided by 2
+ console.log(area);
