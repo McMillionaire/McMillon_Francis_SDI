fb8d1b015b0c413d61469deb50e390feab5dbf26
McMillionaire <McMillionaire@fullsail.edu>
2015-03-11 19:40:12 -0400

    Example Expression
    Using year of birth as example.

---
+++ Expressions-Example/js/script.js
@@ -0,0 +1,11 @@
+
+ /*
+  Francis McMillon
+  SDI 1503
+  Sample Expressions - How old are you?
+  3/11/15
+ */
+var yearBorn = 1959;
+var age = 2015 - yearBorn - 1; //If your birthday has not occurred, you the (-).  If it has, do not use arithmetic operator
+
+ console.log(age);
