95a0c9bdd0c9805dad1078f1a93e5db460f42860
McMillionaire <McMillionaire@fullsail.edu>
2015-03-11 20:02:32 -0400

    Expressions Modulo
    Finding the remainder of an operation

---
+++ Expressions-Modulo/js/script.js
@@ -0,0 +1,14 @@
+
+ /*
+  Francis McMillon
+  SDI 1503
+  Modulo (Finding the remainder)
+  3/11/15
+ */
+
+ var remainder = 32 % 10 //gives you what is leftover when 32 is divided by 10
+
+ //even numbers divided by 2 will all have a remainder of zero
+ //Odd numbers will have a remainder
+
+ console.log(remainder);
