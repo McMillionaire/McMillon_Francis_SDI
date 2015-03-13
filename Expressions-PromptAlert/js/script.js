ed5f8a40ffb853394f4ab0e6ad2f1603b36a281c
McMillionaire <McMillionaire@fullsail.edu>
2015-03-12 22:29:53 -0400

    Expressions Prompt and Alert
    Shows the user their input.  Also the Expressions Assignment ZIPPED.

---
+++ Expressions-PromptAlert/js/script.js
@@ -0,0 +1,18 @@
+
+ /*
+  Francis McMillon
+  SDI 1503
+  Expressions Prompt and Alert (Getting info in and out
+  3/12/15
+ */
+var userInput = prompt("Enter your year of birth");
+console.log(userInput);
+ //
+
+ var width = prompt("We are calculating the area of a rectangle.\nPlease enter the width");
+ var height = prompt("Please enter the height");
+ var area = width * height;
+ console.log(area);
+ //
+
+ alert("The area of your rectangle is " + area + " ")
