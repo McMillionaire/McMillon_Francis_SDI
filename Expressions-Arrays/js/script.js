5a9baef2132fd183c09443ae25146b005c6efe00
McMillionaire <McMillionaire@fullsail.edu>
2015-03-12 20:49:31 -0400

    Expressions Worksheet
    Including arrays and order of operations

---
+++ Expressions-Arrays/js/script.js
@@ -0,0 +1,7 @@
+
+ /*
+  Francis McMillon
+  SDI 1503
+  Expression (Using ARRAYS in Expression
+  3/12/15
+ */
---
+++ Expressions_Worksheet/js/script.js
@@ -0,0 +1,44 @@
+
+ /*
+  Francis McMillon
+  SDI 1503
+  Expressions Worksheet
+  3/11/15
+ */
+
+ //calculate a dog's age
+
+ var human = 3; // Human Years
+ var dog =7; // Dog years
+ var actual = human * dog; //human years times dog years
+
+ console.log(actual);//Sparky is 3 years old, which is 21 in dog years
+
+ //Slice of Pie (Part I)
+
+ var students = 28;//number of students
+ var slices = 8; // slices per pizza
+ var pizza = 11; //pizza ordered
+ var persons = slices * pizza/28;//Number of slices per pizza times the number of pizzas divided by number of students
+ console.log(persons); //Number of slices per person is 3.14
+
+ //Slice of Pie (Part II)
+
+ var students = 28;//number of students
+ var slices = 8; // slices per pizza
+ var pizza = 11; //pizza ordered
+ var persons = slices * pizza/28;//Number of slices per pizza times the number of pizzas divided by number of students
+ console.log(persons); //Sparky will have 1.4 slices of pizza, which is 100 times .014.
+
+ //Average Shopping Bill
+
+ var week1 = 125;//week one shopping bill
+ var week2 = 137;//week two shopping bill
+ var week3 = 101;//week three shopping bill
+ var week4 = 120;//week four shopping bill
+ var week5 = 110;//week five shopping bill
+
+ var total = (week1 + week2 + week3 + week4 + week5)/5; //Total for 5 week period is $593
+ console.log(total);//Spent a total of $593 over 5 week period.  That is an average of $118.6 per week
+
+ //Discounts
