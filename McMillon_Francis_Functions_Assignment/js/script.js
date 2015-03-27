29ccadf29e33ce291fcc1a9126389e3806ed96f1
McMillionaire <McMillionaire@fullsail.edu>
2015-03-26 20:25:38 -0400

    Functions Assginment
    Florida Lottery (PowerBall)

---
+++ McMillon_Francis_Functions_Assignment/js/script.js
@@ -0,0 +1,63 @@
+
+ /*
+  Francis McMillon
+  SDI 1503
+  Functions Assignment - The Florida Lottery (Powerball)
+  3/26/15
+ */
+
+ //My Variables
+ var playDate = prompt("Enter Date of Desired Play, i.e, MO/DAY/YEAR:");
+
+ var quickPicks;
+ var powerBall;
+
+ //My Functions
+ function quickPick(tickNumbers) {
+
+     console.log("You entered: " + playDate);
+
+     var timesClicked = 1;
+
+     while (tickNumbers === "") {
+
+         playDate = prompt("You did not enter a date.\n\nPlease enter your desired date of play! Thanks!!!");
+
+         timesClicked++;
+
+         if (timesClicked === 3) {
+             console.log("Congratulations! We generated your Quick Pick numbers for " + playDate + ". Good Luck!");
+             break;
+         }
+     }
+
+     return tickNumbers;
+
+ }
+
+ function randomNumGen(min, max, num) {
+
+     var randomArray = [];
+
+     //Math.random() * (max - min) + 10
+     for (var i = 0; i < num; i++) {
+
+         var myRandomNumber = Math.round(Math.random() * (max - min) + min);
+
+         randomArray[i] = myRandomNumber;
+
+     }
+
+     return randomArray;
+ }
+
+ //My main code (Output)
+ playDate = quickPick(playDate);
+ console.log("Your date of play is " + playDate + ". Good Luck your numbers are as follows:");
+
+ quickPicks = randomNumGen(1, 59, 5);// 5 random numbers 1 thru 59
+ console.log(quickPicks);
+
+ powerBall = randomNumGen(1, 35, 1);// 1 random number 1 thru 35
+ console.log(powerBall);
+
